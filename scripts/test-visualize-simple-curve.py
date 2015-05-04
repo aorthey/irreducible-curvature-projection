0fc836e1a5bbda6eebdc3465c5b4db96d4514f41
Andreas Orthey <andreas.orthey@gmx.de>
2015-05-04 16:57:34 +0200

    vis in 3d python

--- scripts/test-visualize-simple-curve.py
+++ scripts/test-visualize-simple-curve.py
@@ -6,7 +6,7 @@ from irreducible_sublink_projector import rootLinkToSublinks
 
 if __name__ == '__main__':
         #fname = "../data/TRO_path.txt"
-        fname = "../data/TRO_path.txt"
+        fname = "../data/spheretraj.txt"
         P = rootLinkToSublinks(fname, None)
         deltaT = 0.001
         P.visualizeLinearLinkageProjection(deltaT)
--- scripts/test_one_config.py
+++ scripts/test_one_config.py
@@ -7,6 +7,7 @@ from irreducible_sublink_projector import rootLinkToSublinks
 if __name__ == '__main__':
         #fname = "../data/TRO_path.txt"
         fname = "../data/TRO_path.txt"
+        fname = "../data/spheretraj.txt"
         P = rootLinkToSublinks(fname, None)
-        #P.plotLinearLinkageAtGoal()
-        P.plotLinearLinkageAtTLimits(0.8,-3,0,1,5,-1,1)
+        P.plotLinearLinkageAtGoal()
+        #P.plotLinearLinkageAtTLimits(0.8,-3,0,1,5,-1,1)
